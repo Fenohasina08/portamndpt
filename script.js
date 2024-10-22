8ffa66d0e1c25faf15917f6609f39cbb1e4f707e
fenohasina08 <fenohasina.rafanomezana@student.passerellesnumeriques.org>
2024-10-22 10:22:12 +0300

    vaovao

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ carre.addEventListener("click", () => {
 icon.addEventListener("click", () => {
     liste.classList.remove("active")    
 })
+
+// Fonction pour détecter si un élément est visible à l'écran
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Fonction qui vérifie tous les éléments avec la classe 'fade-in'
+function checkFadeIn() {
+    const elements = document.querySelectorAll('.fade-in');
+    
+    elements.forEach((element) => {
+        if (isInViewport(element)) {
+            element.classList.add('fade-in-visible');
+        }
+    });
+}
+
+// Écouteur d'événements sur le défilement pour déclencher l'animation
+window.addEventListener('scroll', checkFadeIn);
+
+// Pour s'assurer que l'animation s'exécute également lors du chargement de la page
+window.addEventListener('load', checkFadeIn);
+
+
+
